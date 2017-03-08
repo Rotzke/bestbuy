bce26578fea859805b4befd67b17dc6a21ffec21
Nikita Rotsky <kurt.rotzke@gmail.com>
2017-03-09 01:27:49 +0200

    Added main script

---
+++ bestbuy.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""BestBuy API scraper for categories/stores/products data."""
+from io import BytesIO
+from zipfile import ZipFile
+from time import time, sleep
+from datetime import datetime
+from contextlib import closing
+import csv
+import json
+import os
+
+
+from modules import requests
+from modules.clint.textui import progress
+
+API_KEY = ''
+sources = ['categories', 'stores', 'products']
+start_ts = datetime.fromtimestamp(time()).strftime('%Y-%m-%dT%H:%M:%S')
+
+
+def get_data(sources, timing):
+    """Parsing BestBuy data from files."""
+    path = 'Output' + '/' + timing
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError:
+            print('Check filesystem permissions!')
+            exit(1)
+    for source in sources:
+        print('Parsing {}... '.format(source))
+        with BytesIO() as temp, closing(requests.get('''https://'''
+                                                     '''api.bestbuy.com/'''
+                                                     '''v1/{}.json.zip?'''
+                                                     '''apiKey={}'''.
+                                                     format(source, API_KEY),
+                                                     stream=True)) as data:
+            size = int(data.headers.get('content-length'))
+            for chunk in progress.bar(data.iter_content(chunk_size=1024),
+                                      expected_size=(size/1024) + 1):
+                temp.write(chunk)
+            with ZipFile(temp) as z:
+                with open(path + '/' + source + '.csv', 'a') as csv_dump:
+                    with z.open(z.namelist()[0]) as cols:
+                        fieldnames = [i for i in json.loads(cols.read().
+                                                            decode('utf-8')
+                                                            )[0]]
+                        writer = csv.DictWriter(csv_dump,
+                                                fieldnames=fieldnames,
+                                                extrasaction='ignore')
+                        writer.writeheader()
+                    for item in z.namelist():
+                        with z.open(item) as json_data:
+                            [writer.writerow(i)
+                             for i in json.loads(json_data.read().
+                             decode('utf-8'))]
+        sleep(1)
+
+
+if __name__ == '__main__':
+    get_data(sources, start_ts)
